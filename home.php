ca9ed8fa6be49f93a776665af4be81c8eeeddf06
Edgar Cobian <edgarcobian@Edgars-Laptop.local>
2023-01-09 22:08:18 -0800

    This is my first push

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css"> 
+
+    <title>Document</title>
+</head>
+<body>
+<!-- <img class="logo" src="site/logo.png" alt="logo"> -->
+  <div class="main">
+    <main>
+        <img class="logo" src="/phpmotors/images/site/logo.png" alt="logos">
+        <p class="p-count">My Account</p>
+        <nav class="nav">
+            <ul>
+              <li><a href="">Home</a><li>
+              <li><a href="">Classic</a><li>
+              <li><a href="">Sports</a><li>
+              <li><a href="">SUV</a><li>
+              <li><a href="">Truck</a><li>
+              <li><a href="">Used</a><li>
+            </ul>
+          </nav>
+          <p>Content Title Here</p>
+          <div class="footer">
+            <footer>
+              <hr>
+              <div class="first"><p>&copy; PHP Motor, All right reserved".</p>
+              </div>
+              <div class="first">
+                <p>All images used are believed to be "Fair use". Please notify the author if any are not and they will be Last updated:</p>
+              </div>
+              <div  class="first">
+                <p>11 December 2019</p>
+              </div>        
+            </footer>
+          </div>
+    </main>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css"> 
+
+    <title>Document</title>
+</head>
+<body>
+<!-- <img class="logo" src="site/logo.png" alt="logo"> -->
+  <div class="main">
+    <main>
+        <img class="logo" src="/phpmotors/images/site/logo.png" alt="logos">
+        <p class="p-count">My Account</p>
+        <nav class="nav">
+            <ul>
+              <li><a href="">Home</a><li>
+              <li><a href="">Classic</a><li>
+              <li><a href="">Sports</a><li>
+              <li><a href="">SUV</a><li>
+              <li><a href="">Truck</a><li>
+              <li><a href="">Used</a><li>
+            </ul>
+            
+            <p><b>Welcome to PHP Motors!</b></p>
+            <div class="text">  
+              <div class="first-Text">DMC delorean</div>  
+              <div class="second-text">3 Cup holders</div>
+              <div class="second-text">Superman doors</div>
+              <div class="second-text">FUZZY dice!</div>
+            </div>
+            <div>
+            <img class="ownToday" src="/phpmotors/images/site/own_today.png" alt="text_image">
+            <img class="delorean" src="/phpmotors/images/delorean.jpg" alt="car">
+            </div>
+       
+          <div class="footer">
+            <footer>
+              <hr>
+              <div class="first"><p>&copy; PHP Motor, All right reserved".</p>
+              </div>
+              <div class="first">
+                <p>All images used are believed to be "Fair use". Please notify the author if any are not and they will be Last updated:</p>
+              </div>
+              <div  class="first">
+                <p>11 December 2019</p>
+              </div>        
+            </footer>
+          </div>
+    </main>
+  </div>
+</body>
+</html>
